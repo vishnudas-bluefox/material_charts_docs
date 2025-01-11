ab1686ba64d91246f7ca0949e22d8276efebeeb7
vishnudas <vishnudas956783@gmail.com>
2025-01-11 05:52:50 +0530

    config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://vishnudas-bluefox.github.io',
-    base: 'my-repo',
+    base: 'material_charts_docs',
 });
